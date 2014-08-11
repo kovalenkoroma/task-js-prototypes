880088c2529d0e8cad8936d7ada58c98dfcd3f9e
Kovalenko Roman <roma___k@mail.ru>
2014-08-11 23:23:02 +0300

    new version

--- script.js
+++ script.js
@@ -1,44 +1,46 @@
 ﻿        function Monster(){
-            this.bite=function(monstername){
-                if(this==catmonster){
-                    console.log(this.name+ this.typeofdamage + monstername +'. Текущий уровень здоровя '+monstername+ ' равен '+
-                        (birdmonster.currenthp=birdmonster.currenthp-this.sizeofdamage) );
-                }
-                else{
-                    console.log(this.name+ this.typeofdamage + monstername +'. Текущий уровень здоровя '+monstername+ ' равен '+
-                         (catmonster.currenthp=catmonster.currenthp-this.sizeofdamage) );
-                }
+            this.bite=function(monster){
+                monster.currenthp -= this.sizeofdamage;
             }
-            this.sayName=function(){
-                alert(this.name);
+            this.getName=function(){
+                console.log(this.name);
+                return this.name;
             }
         }
-        var monster=new Monster();
+        var monster = new Monster();
 
         function Catmonster(name){
             this.name=name;
             this.starthp=50;
-            this.currenthp=50;
-            this.typeofdamage=' царапать ';
+            this.currenthp=this.starthp;
+            this.typeofdamage='царапать';
             this.sizeofdamage=5;
         }
-        Catmonster.prototype=monster;
-        var catmonster=new Catmonster('Cat');
-        console.log(catmonster);
+        Catmonster.prototype= monster;
 
+        var catmonster=new Catmonster('Cat');
         function Birdmonster(name){
             this.name=name;
             this.starthp=60;
-            this.currenthp=60;
-            this.typeofdamage=' клевать ';
+            this.currenthp=this.starthp;
+            this.typeofdamage='клевать';
             this.sizeofdamage=3;
         }
-        Birdmonster.prototype=monster;
+        Birdmonster.prototype= monster;
         var birdmonster=new Birdmonster('Bird');
+
+        console.log(catmonster);
         console.log(birdmonster);
-        catmonster.bite(birdmonster.name);
-        catmonster.bite(birdmonster.name);
-        catmonster.bite(birdmonster.name);
+
+        catmonster.getName();
+        birdmonster.getName();
+
+        catmonster.bite(birdmonster);
+        console.log(catmonster.name + ' ' + catmonster.typeofdamage +' '+ birdmonster.name);
         console.log(birdmonster);
-        birdmonster.bite(catmonster.name);
+        catmonster.bite(birdmonster);
+        console.log(catmonster.name + ' ' + catmonster.typeofdamage +' '+ birdmonster.name);
+        console.log(birdmonster);
+        birdmonster.bite(catmonster);
+        console.log(birdmonster.name + ' ' + birdmonster.typeofdamage +' '+ catmonster.name);
         console.log(catmonster);
